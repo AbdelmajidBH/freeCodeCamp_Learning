528136ec32643ccede3c3aebea72ac6e59a905d6
loumarven <lou.payot@gmail.com>
2017-10-05 12:29:25 +0800

    adjusted whitespace in algorithm notes

--- Intermediate_Algorithm/smalltestCommons.js
+++ Intermediate_Algorithm/smalltestCommons.js
@@ -5,9 +5,11 @@
   3. Loop while the least common multiple is not found.
   4. In this loop, create another loop to iterate from min up to 1 less than max.
   5. In this inner loop, check if the remainder of multiple and the current number in the inner loop is not zero.
-  6. If the remainder in step 5 is not zero, break out of the inner loop since the current value of multiple is not a multiple of the current number in the inner loop (i). Increment multiple by max so we can try a new multiple.
+  6. If the remainder in step 5 is not zero, break out of the inner loop since the current value of multiple 
+     is not a multiple of the current number in the inner loop (i). Increment multiple by max so we can try a new multiple.
   7. If the remainder in step 5 is zero, check for the next iteration in the inner loop.
-  8. If step 7 is true for all numbers in the range, then the current multiple is the common multiple for all numbers in the range. Return this multiple.
+  8. If step 7 is true for all numbers in the range, then the current multiple is the common multiple 
+     for all numbers in the range. Return this multiple.
 */
 function smallestCommons(arr) {
   var min = Math.min(...arr);
